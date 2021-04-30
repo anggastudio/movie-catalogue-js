9b2d5f446212177cb828c2128e515daf339550a3
anggastudio <aa.angga.pratama@gmail.com>
2021-05-01 00:04:00 +0700

    success settings api and get data

---
+++ src/scripts/globals/config.js
@@ -0,0 +1,8 @@
+const CONFIG = {
+  KEY: 'c6c950e32360be4a417e90d72b380af3',
+  BASE_URL: 'https://api.themoviedb.org/3/',
+  BASE_IMAGE_URL: 'https://image.tmdb.org/t/p/w500/',
+  DEFAULT_LANGUAGE: 'en-us',
+};
+
+export default CONFIG;
